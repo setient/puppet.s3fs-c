e2971ecd755fe8c2cbe2a6aaf4ef1b062673661d
Marcus Pemer <marcus@iteego.com>
2012-11-05 20:48:13 +0100

    ongoing work

--- manifests/init.pp
+++ manifests/init.pp
@@ -86,7 +86,7 @@ class s3fs-c {
       path        => '/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin',
       creates     => '/usr/local/bin/s3fs',
       logoutput   => on_failure,
-      command     => '/etc/puppet/modules/s3fs/files/bin/install.sh',
+      command     => '/etc/puppet/modules/s3fs-c/files/bin/install.sh',
       require     => [
                        Package['pkg-config'],
                        Package['build-essential'],
