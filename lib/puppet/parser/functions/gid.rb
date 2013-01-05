558de1555715a74682de9f2553c2651d23590781
Marcus Pemer <marcus@iteego.com>
2013-01-05 16:02:07 +0100

    We are capturing output of commands, so handle accordingly

--- lib/puppet/parser/functions/gid.rb
+++ lib/puppet/parser/functions/gid.rb
@@ -1,6 +1,6 @@
 module Puppet::Parser::Functions
   newfunction(:gid, :type => :rvalue) do |args|
     raise(Puppet::ParseError, "gid(): Wrong number of arguments given (#{args.size} instead of 1)") if args.size != 1
-    system("sed -nr \"s/^$args[0]:x:([0-9]+):.*/\1/p\" /etc/group").chomp
+    `sed -nr \"s/^$args[0]:x:([0-9]+):.*/\1/p\" /etc/group`
   end
 end
--- lib/puppet/parser/functions/uid.rb
+++ lib/puppet/parser/functions/uid.rb
@@ -1,6 +1,6 @@
 module Puppet::Parser::Functions
   newfunction(:uid, :type => :rvalue) do |args|
     raise(Puppet::ParseError, "uid(): Wrong number of arguments given (#{args.size} instead of 1)") if args.size != 1
-    system("id -u $args[0]").chomp
+    `id -u $args[0]`
   end
 end
