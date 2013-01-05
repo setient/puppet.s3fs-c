a9a94bd0b2b7389602786ba9bacaed2cbb4b68a5
Marcus Pemer <marcus@iteego.com>
2013-01-05 16:29:56 +0100

    Attempting an alternate approach that hopefully will execute correctly from ruby

--- lib/puppet/parser/functions/gid.rb
+++ lib/puppet/parser/functions/gid.rb
@@ -1,6 +1,6 @@
 module Puppet::Parser::Functions
   newfunction(:gid, :type => :rvalue) do |args|
     raise(Puppet::ParseError, "gid(): Wrong number of arguments given (#{args.size} instead of 1)") if args.size != 1
-    `sed -nr "s/^#{args[0]}:x:([0-9]+):.*/\1/p" /etc/group`.chomp
+    `grep ^#{args[0]}: /etc/group | cut -d: -f3`.chomp
   end
 end
