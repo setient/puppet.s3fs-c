f8269eb8915930817edcfc4aea80a8edd73346f7
Marcus Pemer <marcus@iteego.com>
2013-01-05 16:04:54 +0100

    We are capturing output of commands, so handle accordingly

--- lib/puppet/parser/functions/uid.rb
+++ lib/puppet/parser/functions/uid.rb
@@ -1,6 +1,6 @@
 module Puppet::Parser::Functions
   newfunction(:uid, :type => :rvalue) do |args|
     raise(Puppet::ParseError, "uid(): Wrong number of arguments given (#{args.size} instead of 1)") if args.size != 1
-    `id -u $args[0]`
+    `id -u $args[0]`.chomp
   end
 end
