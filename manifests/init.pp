3cb871d96ea70d35a94dd411ecab5b6140263029
Marcus Pemer <marcus@iteego.com>
2013-03-25 17:05:13 +0100

    Corrected sed command

--- manifests/init.pp
+++ manifests/init.pp
@@ -30,7 +30,7 @@ class s3fs-c {
         exec { "/bin/echo '${bucket}:${key}:${secret}' >> '${file}'":
           unless => "/bin/grep -q '^${bucket}:' '${file}'",
         }
-        exec { "/bin/sed -i '' -e 's/${bucket}:.*:.*/${bucket}:${key}:${secret}' '${file}'":
+        exec { "/bin/sed 's/${bucket}:.*:.*/${bucket}:${key}:${secret}/' '${file}'":
           unless => "/bin/grep -q '^${bucket}:${key}:${secret}' '${file}'",
         }
       }
